5001a7f135c86b17ea7d8978cafdf73578a9a8b1
Tom Nielsen <tanielsen@gmail.com>
2021-04-29 17:21:43 +0100

    basic server

---
+++ server.py
@@ -0,0 +1,20 @@
+from flask import flash
+from flask import Flask
+from flask import redirect
+from flask import render_template
+from flask import request
+from flask import url_for
+from flask_sqlalchemy import SQLAlchemy
+
+from server.config import get_db_uri
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = get_db_uri()
+db = SQLAlchemy(app)
+
+@app.route("/")
+def hello():
+    return 'Hello, World!'
+
+if __name__ == "__main__":
+    app.run()
