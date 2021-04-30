a6fc19561e93e11d94d1e9768fb119ecaaba0ab9
Tom Nielsen <tanielsen@gmail.com>
2021-04-30 12:32:00 +0100

    with pipeline

--- train_prediction.py
+++ train_prediction.py
@@ -12,25 +12,30 @@ print(get_db_uri())
 
 engine = create_engine(get_db_uri())
 
-df = pd.read_sql_query('SELECT * FROM "Product" where price is not null', con=engine)
+df = pd.read_sql_query('SELECT category, price FROM "Product" where price is not null', con=engine)
 df_y = df['price']
-print(df_y)
-dums=pd.get_dummies(df["category"])
+#print(df)
+#dums=pd.get_dummies(df["category"])
 #print(dums)
 
 df_cat = df[['category']]
 
-preprocessor = ColumnTransformer(
-    transformers=[('cat', OneHotEncoder(handle_unknown='ignore'), ['category'])])
+categorical_features = ['embarked', 'sex', 'pclass']
+categorical_transformer = OneHotEncoder(handle_unknown='ignore')
 
-#xform = Pipeline(steps=[('preprocessor', preprocessor)]).fit_transform(df_x)
-xform = preprocessor.fit_transform(df)
+preprocessor = ColumnTransformer(
+    transformers=[
+        #('num', numeric_transformer, numeric_features),
+        ('cat', OneHotEncoder(handle_unknown='ignore'), ['category'])])
+
+# Append classifier to preprocessing pipeline.
+# Now we have a full prediction pipeline.
+clf = Pipeline(steps=[('preprocessor', preprocessor),
+                      ('classifier', LinearRegression())])
 
 # Append classifier to preprocessing pipeline.
 # Now we have a full prediction pipeline.
 #xform = Pipeline(steps=[('preprocessor', preprocessor)]).fit_transform(df_x)
-
-print(xform)
-model = LinearRegression()
-model.fit(dums, df_y)
-print(model)
+clf.fit(df_cat, df_y)
+pred=clf.predict(pd.DataFrame.from_dict({'category': ["Clothing"]}))
+print(pred)
